1c8e2ccb41d4a232625ca596b96212a892aaad54
STEVENBOBER <spbober@yahoo.com>
2024-12-07 09:22:31 -0800

    rust_setup

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world. I'm a rustcean!");
+
+    let x = 5 + /* 90 + */ 5;
+    println!("Is `x` 10 or 100? x = {}", x);
+
+    let y = 6 + 6;
+    println!("Is 'y' 12 or 120? y = {}", y)
+}
